7892067e0c9368e6f90a085a3a5f21efdefafc7e
sasha <lebedev2008.a@gmail.com>
2023-10-12 16:26:57 +0300

    First commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- Basic config
+require("core.mappings")
+require("core.plugins")
+require("core.colors")
+require("core.configs")
+
+-- Plugins config
+require("plugins.telescope")
+require("plugins.lsp")
+require("plugins.mason")
+require("plugins.cmp")
+require("plugins.treesitter")
+require("plugins.neotree")
+
